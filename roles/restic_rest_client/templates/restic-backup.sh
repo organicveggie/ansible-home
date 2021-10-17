5f359f1a59874c88f73b7692efd45466fda0616a
Sean Laurent <organicveggie@gmail.com>
2021-10-17 09:45:38 -0700

    Create restic backup script

---
+++ roles/restic_rest_client/templates/restic-backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu
+
+# Load repository config via environment variables
+source '/etc/restic/restic-env.sh'
+
+echo "Starting restic backup"
+
+# Check if repository is okay
+restic check
+
+# Create new backup
+restic backup \
+       --one-file-system \
+       --exclude-caches \
+       --files-from '{{ restic_client_backup_includes_file }}'
+       --exclude-file '{{ restic_client_backup_excludes_file }}'
+       --tag automated
+
+echo "Finished restic backup"
+
+exit 0
