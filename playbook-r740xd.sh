2175c91d7cc19f549991bf0509633b083c93680a
Sean Laurent <organicveggie@gmail.com>
2026-07-03 06:08:32 +0000

    Configure Mosquitto with dynamic security and users
    Add initial roles and permissions for Zigbee2MQTT to publish and subscribe to topics.

--- playbook-r740xd.sh
+++ playbook-r740xd.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-TAGS=$1
+TAGS="$1"
 
 if [ -z "${TAGS}" ]
 then
  echo "Missing required tags parameter"
  exit 1
 fi
+shift
 
 set -x
-ansible-playbook -K --vault-password-file vault-passwd -l r740xd playbooks/r740xd.yml -t ${TAGS}
+ansible-playbook -K --vault-password-file vault-passwd -l r740xd playbooks/r740xd.yml -t ${TAGS} "$@"
 
 set +x
